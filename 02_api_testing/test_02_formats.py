18308b14b9b62d2ec78f941c15cded00d9756ef8
Andrey Gridyaev <agridyaev@gmail.com>
2020-11-18 05:36:22 -0500

    test_02_formats: use https://httpbin.org

--- 02_api_testing/test_02_formats.py
+++ 02_api_testing/test_02_formats.py
@@ -1,15 +1,15 @@
 import xml.etree.ElementTree as ET
 
 
-def test_api_json(session, base_url):
-    r = session.get(f'{base_url}/json')
+def test_api_json(session):
+    r = session.get('https://httpbin.org/json')
     r_json = r.json()
     assert r_json['slideshow']['author'] == "Yours Truly"
     assert r_json['slideshow']['title'] == "Sample Slide Show"
 
 
-def test_api_xml(session, base_url):
-    r = session.get(f'{base_url}/xml')
+def test_api_xml(session):
+    r = session.get('https://httpbin.org/xml')
     root = ET.fromstring(r.content)
     assert root.tag == "slideshow"
     assert root.attrib['author'] == "Yours Truly"

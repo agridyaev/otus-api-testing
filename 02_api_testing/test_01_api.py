f21b1509fad054c635d7ed348266f0f7b0b0f3be
Andrey Gridyaev <agridyaev@gmail.com>
2020-12-06 12:24:25 +0300

    test_01_api: small improvements

--- 02_api_testing/test_01_api.py
+++ 02_api_testing/test_01_api.py
@@ -16,38 +16,34 @@ def test_get_negative(session, base_url, post_id):
     res = session.get(url=f'{base_url}/{post_id}')
 
     assert res.status_code == 404
-    assert not res.json()
+    assert len(res.json()) == 0
 
 
 def test_create(session, base_url):
-    title = 'foo'
-    body = 'bar'
-    payload = {'title': title, 'body': body, 'userId': 1}
+    payload = {'title': 'foo', 'body': 'bar', 'userId': 1}
     res = session.post(url=base_url, json=payload)
 
     assert res.status_code == 201
     j = res.json()
     assert j['id'] == POSTS_MAX + 1
-    assert j['userId'] == 1
-    assert j['title'] == title
-    assert j['body'] == body
+    assert j['userId'] == payload['userId']
+    assert j['title'] == payload['title']
+    assert j['body'] == payload['body']
 
 
 def test_update(session, base_url):
-    post_id = 1
-    title = 'foo'
-    body = 'bar'
-    payload = {'title': title, 'body': body, 'id': post_id, 'userId': 1}
-    res = session.put(url=f'{base_url}/{post_id}', json=payload)
+    payload = {'title': 'foo', 'body': 'bar', 'id': 1, 'userId': 1}
+    res = session.put(url=f"{base_url}/{payload['id']}", json=payload)
 
     assert res.status_code == 200
     res_json = res.json()
-    assert res_json['title'] == title
-    assert res_json['body'] == body
+    assert res_json['title'] == payload['title']
+    assert res_json['body'] == payload['body']
+    assert res_json['userId'] == payload['userId']
 
 
 def test_delete(session, base_url):
     res = session.delete(url=f'{base_url}/1')
 
     assert res.status_code == 200
-    assert not res.json()
+    assert len(res.json()) == 0
